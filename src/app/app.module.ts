b909ce6593cfe603da7ab37b355a71324bbc5540
Kashaf shanawar <kashaf.shanawar@gmail.com>
2024-10-02 17:25:12 +0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';  // Import Header Component
-import { FooterComponent } from './footer/footer.component'; 
+import { HeaderComponent } from './home-page/header/header.component';  // Import Header Component
+import { FooterComponent } from './home-page/footer/footer.component'; 
 
 @NgModule({
   declarations: [
