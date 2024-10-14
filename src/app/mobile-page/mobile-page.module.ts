0c3770040105a25b36a4e28c1fb557d4e364b3cd
Kashaf shanawar <kashaf.shanawar@gmail.com>
2024-10-14 18:15:06 +0500

    sign in

--- src/app/mobile-page/mobile-page.module.ts
+++ src/app/mobile-page/mobile-page.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MobileScreenComponent } from './mobile-screen/mobile-screen.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MobileScreenComponent
+  ],
   imports: [
     CommonModule
   ]
