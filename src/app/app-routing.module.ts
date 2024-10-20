dd2b10debf42309365c492275db7f1d62a546b81
arsalan139 <88947632+arsalan139@users.noreply.github.com>
2024-10-20 20:31:12 +0500

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { VendorComponent } from './vendor-page/vendor/vendor.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
   { path: 'sign-in', component: SignInComponent },
   { path: 'vendor/:vendorName', component: VendorComponent }
 ];
